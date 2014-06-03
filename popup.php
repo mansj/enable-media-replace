02d35c69710c4babb0252a7c06583e5901472ab0
jeremyfelt <jeremy.felt@gmail.com>
2014-06-03 14:24:34 -0700

    Add an emr_display_replace_type_options filter
    This filter will allow others to decide if options should be
    available when uploading a new image rather than allowing the
    default behavior to just happen.

--- popup.php
+++ popup.php
@@ -49,6 +49,7 @@ $current_filename = substr($current_filename, (strrpos($current_filename, "/") +
 
 		<input type="file" name="userfile" />
 
+	<?php if ( apply_filters( 'emr_display_replace_type_options', true ) ) : ?>
 		<p><?php echo __("Select media replacement type:", "enable-media-replace"); ?></p>
 
 		<label for="replace_type_1"><input CHECKED id="replace_type_1" type="radio" name="replace_type" value="replace"> <?php echo __("Just replace the file", "enable-media-replace"); ?></label>
@@ -57,8 +58,9 @@ $current_filename = substr($current_filename, (strrpos($current_filename, "/") +
 		<label for="replace_type_2"><input id="replace_type_2" type="radio" name="replace_type" value="replace_and_search"> <?php echo __("Replace the file, use new file name and update all links", "enable-media-replace"); ?></label>
 		<p class="howto"><?php echo __("Note: If you check this option, the name and type of the file you are about to upload will replace the old file. All links pointing to the current file (", "enable-media-replace"); ?><?php echo $current_filename; ?><?php echo __(") will be updated to point to the new file name.", "enable-media-replace"); ?></p>
 		<p class="howto"><?php echo __("Please note that if you upload a new image, only embeds/links of the original size image will be replaced in your posts."); ?></p>
-
+	<?php else : ?>
+		<input type="hidden" name="replace_type" value="replace" />
+	<?php endif; ?>
 		<input type="submit" class="button" value="<?php echo __("Upload", "enable-media-replace"); ?>" /> <a href="#" onclick="history.back();"><?php echo __("Cancel", "enable-media-replace"); ?></a>
-
 	</form>
 </div>
