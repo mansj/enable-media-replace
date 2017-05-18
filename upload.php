88a591b0fa5f0cb04fb24319a56c73cfc25dc948
John Pariseau <simplymail@ururk.com>
2017-05-17 22:16:19 -0400

    Make $returnurl filterable, use admin_url

--- upload.php
+++ upload.php
@@ -184,7 +184,7 @@ if (is_uploaded_file($_FILES["userfile"]["tmp_name"])) {
 
 	}
 
-	$returnurl = get_bloginfo("wpurl") . "/wp-admin/post.php?post={$_POST["ID"]}&action=edit&message=1";
+	$returnurl = admin_url("/post.php?post={$_POST["ID"]}&action=edit&message=1");
 	
 	// Execute hook actions - thanks rubious for the suggestion!
 	if (isset($new_guid)) { do_action("enable-media-replace-upload-done", ($new_guid ? $new_guid : $current_guid)); }
@@ -192,12 +192,15 @@ if (is_uploaded_file($_FILES["userfile"]["tmp_name"])) {
 } else {
 	//TODO Better error handling when no file is selected.
 	//For now just go back to media management
-	$returnurl = get_bloginfo("wpurl") . "/wp-admin/upload.php";
+	$returnurl = admin_url("/wp-admin/upload.php");
 }
 
 if (FORCE_SSL_ADMIN) {
 	$returnurl = str_replace("http:", "https:", $returnurl);
 }
+
+// Allow developers to override $returnurl
+$returnurl = apply_filters('emr_returnurl', $returnurl);
 
 //save redirection
 wp_redirect($returnurl);
